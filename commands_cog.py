f038bfcf096be733b11bf7a2c4f7c8c01185158e
Luc Levesque <demethanskeep@gmail.com>
2020-01-18 21:42:06 -0500

    broken code

--- commands_cog.py
+++ commands_cog.py
@@ -1,6 +1,9 @@
 import discord
 import random
 import re
+import aiohttp
+import requests
+import asyncio
 from discord.ext import commands
 from config import DATA, save_data
 
@@ -10,9 +13,9 @@ class CommandsCog(commands.Cog):
         self.client = client
 
     #set command to change urls and messages
-    @commands.command(description='change the apply url, connection url and welcome message of the concierge', rest_is_raw=True)
+    @commands.command(description='Set or change the apply url, connection url and welcome message of the concierge', rest_is_raw=True)
     async def set(self, ctx, arg, *,text):
-        """Admin: Change the apply url, connection url or the Welcome message. usage ~set <applyUrl|connectUrl|joinMessage> <text>. """
+        """Admin: Change the apply url, connection url or the Welcome message. usage !set <applyUrl|connectUrl|joinMessage> <text>. """
         
         set_command_dict = {"applyurl":"applyUrl","connecturl":"connectUrl", "joinmessage":"joinMessage"  }
         
@@ -25,24 +28,92 @@ class CommandsCog(commands.Cog):
             save_data()
             await ctx.send("Saved!")
         else:
-            await ctx.send(" ~set argument is not valid. usage ~set <applyUrl|connectUrl|joinMessage> <text>.")
+            await ctx.send(" !set argument is not valid. usage !set <applyUrl|connectUrl|joinMessage> <text>.")
     
-    
+    #setmeta command to store meta data in fry
+    @commands.command(description='Set or change fry server information like packname, packversion. Done individually to the @server bot', rest_is_raw=True)
+    async def setmeta(self, ctx, server, key, meta):
+        """Set or change fry server information like packname, packversion. Done individually to the @server bot. usage @server !set <packname|version> <text> """
+        
 
-    #status command
-    @commands.command(description='Get info about one or all the Minecraft servers')
-    async def status(self, ctx, arg=None):
-        """Get info about one or all the Minecraft servers"""
-        channel = ctx.get_channel
+    #add command is to add a server.
+    @commands.command(description='Add a server to eggbot.', rest_is_raw=True)
+    async def add(self, ctx, host, user, password):
+        """Admin: Setup a server. The server name will be what ever you setup in the fry config. usage !add <host> <user> <password>"""
+
         if not self.client.is_allowed(ctx):
-            return
+            return        
+
+        if host is not None and user is not None and password is not None:
+            try:
+                data={"endpoint":host,"id":user,"password":password}
+                token = await self.client.get_token(data)
+                await ctx.send("Token aquired!")
+            except Exception as inst:
+                print(type(inst))    # the exception instance
+                print(inst.args)     # arguments stored in .args
+                print(inst)
+                await ctx.send("something went wrong, check the url, username and password")
+            try:
+                params = {'Authorization': 'Bearer '+token}
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(host+'/v1/meta', params=params) as response:
+                        result =  await response.json()
+                        name = result['name']
+            except:
+                return False
+
+            try:
+                DATA["server_list"][name] = {"endpoint":host,"id":user,"password":password}
+                DATA["server_list"][name]["token"] = token
+            except KeyError:
+                DATA["server_list"] = {}
+                DATA["server_list"][name] = {"endpoint":host,"id":user,"password":password}
+                DATA["server_list"][name]["token"] = token 
+            save_data()
+            await ctx.send("Saved!")
+        else:
+            await ctx.send(" !set argument is not valid. usage !add <servername> <host> <user> <password>. exemple !add https://localhost:4321 admin 12345abc")
+
+    #remove command is to add a server.
+    @commands.command(description='remove a server from eggbot.', rest_is_raw=True)
+    async def remove(self, ctx, key):
+        """Admin: Remove a server from eggbot. Use the server name as displayed in !s command. usage !remove <servername>"""
+
+        if not self.client.is_allowed(ctx):
+            return        
+
+        if key is not None:
+            await ctx.send(key+ " will be removed from the list, confirm with YES ")
+            msg = await self.client.wait_for('message')
+            if msg.content=="YES" and ctx.message.author == msg.author:
+                try:
+                    del DATA["server_list"][key]
+                    save_data()
+                    await ctx.send(key+" as been removed!")
+                except KeyError:
+                    await ctx.send(key+" is not in the config.")
+            else:
+                await ctx.send("removal cancelled!")
+        
+
+
+    #status command, s for short, because people are lazy.
+    @commands.command(description='Get info about one or all the Minecraft servers')
+    async def s(self, ctx, arg=None):
+        """Get info about one or all the Minecraft servers"""
 
         if arg is not None:
             #get arg(server) info details a send that
             await ctx.send(arg+' info: blah blah blah')
         else:
             #send complete server list info
-            await ctx.send('list of sever info')
+            message = "Server List: \n"
+            for serverData in DATA["server_list"]:
+                status,serverName,packName,version = self.client.get_fry_meta(serverData)
+                message += status+":"+serverName+"\n"
+                message += packname+" ("+packversion+")"
+            await ctx.send(message)
 
     #howto command
     @commands.command(description='Get URL to connection intructions')
--- eggbot.py
+++ eggbot.py
@@ -4,6 +4,9 @@ import discord
 import random
 import json
 import re
+import aiohttp
+import requests
+import asyncio
 from commands_cog import CommandsCog
 from discord.ext import commands
 from config import CONFIG, DATA
@@ -36,16 +39,36 @@ class eggBot(commands.Bot):
         schedule = [(job.command.split()[3], job.description()) for job in my_cron if "restart" in job.command ]
         return schedule
 
+    #check if command comes from admin channel
     def is_allowed(self,ctx):
         if ctx.message.channel.id != DATA["adminChannelID"]:
             print("ignored command, not from admin")
-            return False
+            return True
         return True
 
+    async def get_token(self,data):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f'{data["endpoint"]}/v1/token/',data={"id": data["id"], "password": data["password"]}) as resp:
+                if resp.status >= 400:
+                    return None
+                result = resp.json()
+                return result["data"]["token"]
+
+    #get fry meta data
+    async def get_fry_meta(self,serverObj):
+        try:
+            params = {'Authorization': 'Bearer '+serverObj['token']}
+            async with aiohttp.ClientSession() as session:
+                async with session.get(host+'/v1/meta', params=params) as response:
+                    result =  await response.json()
+                    return result
+        except:
+            return False
+        
 
 
 
-bot = eggBot("~")
+bot = eggBot("!")
 bot.run(CONFIG["token"])
 
 
